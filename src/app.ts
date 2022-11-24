60c4235a454ed626d61a72057e32079bfbf5c8be
Jesus Narbona <jesusnarbona2001@gmail.com>
2022-11-24 21:23:56 +0100

    feat: main app

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import { ProductsService } from "./services/products.service";
+import { ProductsView } from "./views/products.view";
+import { ProductsController } from "./controllers/products.controller";
+import { CartView } from "./views/cart.view";
+import { CartService } from "./services/cart.service";
+import { CartController } from "./controllers/cart.controller";
+
+const productsService = new ProductsService();
+const productsView = new ProductsView();
+const cartView = new CartView();
+const cartService = new CartService();
+const productController = new ProductsController(productsView, productsService);
+const cartController =new CartController(cartView, cartService,productsView,productsService);
